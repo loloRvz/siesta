7a04b7cb74cc8335eef790708a1ad67c5bcfbe81
loloRvz <rouvinez.laurent@gmail.com>
2023-05-30 14:08:42 +0200

    new data

--- scripts/siesta_sysid.py
+++ scripts/siesta_sysid.py
@@ -138,10 +138,10 @@ def main():
 
     # Open training dataset
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    list_of_files = glob.glob(dir_path + '/../data/experiments/training/*.csv')
+    list_of_files = glob.glob(dir_path + '/../data/training/*.csv')
     list_of_files = sorted(list_of_files)
     list_of_files.reverse()
-    path = list_of_files[2]
+    path = list_of_files[0]
     print("Opening: ",path)
 
     # Prepare dataset
